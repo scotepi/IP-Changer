6f8b107517839de283731f835e5ec512ae612aa8
Scott Peters <scotepi@gmail.com>
2017-05-19 21:45:10 -0400

    Removed checkbox for DHCP DNS because I can't currently find it in the code and it is a seldum used feature

--- IP Changer/Form1.cs
+++ IP Changer/Form1.cs
@@ -64,16 +64,60 @@ namespace IP_Changer
 
                     if (ip.GatewayAddresses.Count > 0)
                         inputGateway.Text = ip.GatewayAddresses[0].Address.ToString();
+                    else
+                        inputGateway.Text = "";
 
                     if (ip.DnsAddresses.Count > 0)
                         inputDNS1.Text = ip.DnsAddresses[0].ToString();
+                    else
+                        inputDNS1.Text = "";
 
                     if (ip.DnsAddresses.Count > 1)
                         inputDNS2.Text = ip.DnsAddresses[1].ToString();
+                    else
+                        inputDNS2.Text = "";
+
+
+                    foreach (UnicastIPAddressInformation ipUni in ip.UnicastAddresses)
+                    {
+                        if (ipUni.Address.AddressFamily.ToString() == "InterNetwork")
+                        {
+
+                            inputIP.Text = ipUni.Address.ToString();
+
+                            try
+                            {
+                                inputNetmask.Text = ipUni.IPv4Mask.ToString();
+                            } catch
+                            {
+                                inputNetmask.Text = "";
+                            }
+                                
+
+                            break;
+                        } else
+                        {
+                            inputIP.Text = "";
+                            inputNetmask.Text = "";
+                        }
+                    }
+
+                    try
+                    {
+                        IPv4InterfaceProperties ipv4 = ip.GetIPv4Properties();
+
+                        if (ipv4.IsDhcpEnabled.ToString() == "True")
+                            checkboxDHCPIP.Checked = true;
+                        else
+                            checkboxDHCPIP.Checked = false;
+                    }
+                    catch
+                    {
+                        checkboxDHCPIP.Checked = false;
+                    }
+                    
                 }
             }
         }
-
-
     }
 }
